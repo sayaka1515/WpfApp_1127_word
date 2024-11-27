1e24dc719f2162395439ee1786bb8e04f294eb7e
英豪 李 <5b1g0009@stust.edu.tw>
2024-11-27 11:31:00 +0800

    新增 WPF 工具包並更新工具欄和命令綁定
    新增了 `xmlns:xctk` 命名空間以使用擴展的 WPF 工具包。
    在 `Window.CommandBindings` 中新增了 `Open` 和 `Save` 命令的綁定。
    更新了 `ToolBar`，增加了 `Open`、`Save` 和 `Clear` 按鈕，並調整了按鈕的大小和圖像。
    新增了一個 `ToolBar`，包含 `Bold`、`Italic`、`Underline` 的切換按鈕，以及字體顏色選擇器、字體系列和字體大小的下拉選單。
    新增了一個 `RichTextBox`，用於顯示和編輯文檔內容。
    在 `MyDocumentViewer.xaml.cs` 中新增了 `System.Runtime.Serialization`、`Microsoft.Win32` 和 `System.IO` 的引用。
    新增了 `OpenCommand_Executed` 和 `SaveCommand_Executed` 方法，用於打開和保存文檔。
    新增了 `documentRichTextBox_SelectionChanged` 方法，用於處理選擇變更事件，更新工具欄按鈕和選擇器的狀態。
    新增了 `fontSizeComboBox_SelectionChanged`、`fontFamilyComboBox_SelectionChanged` 和 `fontColorPicker_SelectedColorChanged` 方法，用於處理字體大小、字體系列和字體顏色的變更。
    新增了 `clearButton_Click` 方法，用於清除 `RichTextBox` 的內容。
    在項目文件 `WpfApp_1120_2.csproj` 中新增了 `Extended.Wpf.Toolkit` 的包引用。
    更新了項目文件中的資源配置，將 `bold.png`、`diskette.png`、`folder.png`、`italic.png`、`trash.png` 和 `underline.png` 圖片設置為資源。

--- WpfApp_1120_2/MyDocumentViewer.xaml.cs
+++ WpfApp_1120_2/MyDocumentViewer.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WpfApp_1120_2
 {
@@ -19,15 +22,108 @@ namespace WpfApp_1120_2
     /// </summary>
     public partial class MyDocumentViewer : Window
     {
+        Color fontColor = Colors.Black;
         public MyDocumentViewer()
         {
             InitializeComponent();
+            fontColorPicker.SelectedColor = fontColor;
+            foreach (FontFamily fontFamily in Fonts.SystemFontFamilies)
+            {
+                fontFamilyComboBox.Items.Add(fontFamily.Source);
+            }
+            fontFamilyComboBox.SelectedIndex = 8;
+
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            fontSizeComboBox.SelectedIndex = 2;
         }
 
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
             myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            openFileDialog.DefaultExt = ".rtf";
+            openFileDialog.AddExtension = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                TextRange range = new TextRange(documentRichTextBox.Document.ContentStart, documentRichTextBox.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            saveFileDialog.DefaultExt = ".rtf";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange range;
+                FileStream fileStream;
+                range = new TextRange(documentRichTextBox.Document.ContentStart, documentRichTextBox.Document.ContentEnd);
+                fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                range.Save(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
+        }
+
+        private void documentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var property_bold = documentRichTextBox.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldToggleButton.IsChecked = (property_bold != DependencyProperty.UnsetValue) && (property_bold.Equals(FontWeights.Bold));
+
+            var property_italic = documentRichTextBox.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicToggleButton.IsChecked = (property_italic != DependencyProperty.UnsetValue) && (property_italic.Equals(FontStyles.Italic));
+
+            var property_underline = documentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineToggleButton.IsChecked = (property_underline != DependencyProperty.UnsetValue) && (property_underline.Equals(TextDecorations.Underline));
+
+            var property_fontSize = documentRichTextBox.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            fontSizeComboBox.SelectedItem = property_fontSize;
+
+            var property_fontFamily = documentRichTextBox.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            fontFamilyComboBox.SelectedItem = property_fontFamily.ToString();
+
+            var property_fontColor = documentRichTextBox.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+            fontColorPicker.SelectedColor = ((SolidColorBrush)property_fontColor).Color;
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                documentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            }
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox.SelectedItem != null)
+            {
+                documentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, new FontFamily((string)fontFamilyComboBox.SelectedItem));
+            }
+        }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = fontColorPicker.SelectedColor.Value;
+            SolidColorBrush brush = new SolidColorBrush(fontColor);
+            documentRichTextBox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, brush);
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            documentRichTextBox.Document.Blocks.Clear();
         }
     }
 }
